2c35014490abffb803f508ab7f076179d797f935
Blujaytech <blujaytechnlogies@gmail.com>
2026-09-26 15:46:51 +0530

    code-enhanced

--- 60-ingress-alb/provider.tf
+++ 60-ingress-alb/provider.tf
@@ -2,15 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.84.0"
+      version = "6.28.0"
     }
   }
 
   backend "s3" {
-    bucket = "82s-tf-remote-state-dev"
+    bucket = "82s-tf-remote-state-dev-hyd"
     key    = "expense-dev-web-alb" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
-    dynamodb_table = "82s-tf-remote-state-dev"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
