a05c935a6191ab61ee13d560e9b586e53e58544a
Blujaytech <blujaytechnlogies@gmail.com>
2026-09-26 12:56:11 +0530

    ansible- end to end playbooks

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "82s-tf-remote-state-dev-hyd"
+    key    = "expense-dev-eks-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
